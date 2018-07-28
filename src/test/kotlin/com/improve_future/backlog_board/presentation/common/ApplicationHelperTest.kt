5323e4580a2b64612634025155309e65eaf9b94d
Kenji Otsuka <kok.fdcm@gmail.com>
2018-07-29 06:11:27 +0900

    upgrade gradle

--- src/test/kotlin/com/improve_future/backlog_board/presentation/common/ApplicationHelperTest.kt
+++ src/test/kotlin/com/improve_future/backlog_board/presentation/common/ApplicationHelperTest.kt
@@ -1,16 +1,9 @@
 package com.improve_future.backlog_board.presentation.common
 
-import kotlinx.html.FlowContent
-import kotlinx.html.TagConsumer
 import kotlinx.html.body
-import kotlinx.html.dom.create
 import kotlinx.html.dom.createHTMLDocument
-import kotlinx.html.dom.document
 import kotlinx.html.html
-import kotlinx.html.stream.appendHTML
-import org.junit.Test
-import java.io.StringWriter
-import javax.management.Query.div
+import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 
